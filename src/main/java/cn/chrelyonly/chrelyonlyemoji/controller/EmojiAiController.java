a0a04533379bc88d222bf2bee124cade21f68d54
chrelyonly <1172576293@qq.com>
2025-06-04 14:52:45 +0800

    1

--- src/main/java/cn/chrelyonly/chrelyonlyemoji/controller/EmojiAiController.java
+++ src/main/java/cn/chrelyonly/chrelyonlyemoji/controller/EmojiAiController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,7 +37,7 @@ public class EmojiAiController {
     @RequestMapping("/gif")
     public void gif(HttpServletResponse response) {
         String gifPath = "D:\\dev\\dev\\project\\chrelyonly-emoji\\resource\\1.gif";
-        String avatarPath = "D:\\dev\\dev\\project\\chrelyonly-emoji\\resource\\2.jpg";
+        String avatarPath = "D:\\dev\\dev\\project\\chrelyonly-emoji\\resource\\4.jpg";
         byte[] result = gifTextService.replaceGifFace(
                 Files.readAllBytes(Paths.get(gifPath)),
                 Files.readAllBytes(Paths.get(avatarPath))
@@ -45,6 +46,29 @@ public class EmojiAiController {
         response.getOutputStream().write(result);
         response.getOutputStream().flush();
 //        return R.ok();
+    }
+
+    @SneakyThrows
+    @RequestMapping("/uploadEmoji")
+    public R uploadEmoji(HttpServletResponse response,@RequestBody Map<String, String> body) {
+        String gifPath = "D:\\dev\\dev\\project\\chrelyonly-emoji\\resource\\1.gif";
+        String base64 = body.get("base64");
+        // 去掉前缀：data:image/png;base64,
+        if (base64.contains(",")) {
+            base64 = base64.split(",")[1];
+        }
+        // ✅ 这是正确的处理方式
+        byte[] avatar = Base64.getDecoder().decode(base64);
+        byte[] result = gifTextService.replaceGifFace(
+                Files.readAllBytes(Paths.get(gifPath)),
+                avatar);
+//        response.setContentType("image/gif");
+//        response.getOutputStream().write(result);
+//        response.getOutputStream().flush();
+
+        // 转成base64字符串
+        String gifBase64 = Base64.getEncoder().encodeToString(result);
+        return R.data(gifBase64);
     }
 
 }
